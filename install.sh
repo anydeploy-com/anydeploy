1632a86b5ca7f25cd2136dbf180219d73bd74ece
anydeploy-com <martin@anydeploy.com>
2018-05-01 16:05:15 +0100

    added notes

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-#Single file installer goes here.
+# Single file installer  will go here.
 
-#
+# Setup Folders
+
+# Setup user, user variables
+
+# Fix permissions
+
+# Download virtio iso
+
+# Download dependencies
+
+
+# Compile iPXE
+
+# Install TFTP Server
+
+# Inatall IPXE
+
+# Install DHCP / Explain how to setup dhcp + install netboot part
+
+# Install LEMP Stack
