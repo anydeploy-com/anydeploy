5a89973708836cf72bc9a82c2e230aeb0d192b3a
anydeploy-com <martin@anydeploy.com>
2018-05-24 15:57:08 +0100

    testing_spinner

---
+++ scripts/spinner_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source "$(pwd)/bash-spinner/spinner.sh"
+
+# test success
+start_spinner 'sleeping for 2 secs...'
+sleep 2
+stop_spinner $?
+
+# test fail
+start_spinner 'copying non-existen files...'
+# use sleep to give spinner time to fork and run
+# because cp fails instantly
+sleep 1
+cp 'file1' 'file2' > /dev/null 2>&1
+stop_spinner $?
+
+start_spinner "updating os..."
+apt-get update > /anydeploy/tmp/update
+stop_spinner $?
