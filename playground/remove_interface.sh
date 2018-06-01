06525296bb16c9d31049382f5f17d1c4c972d0f8
martin <martin@ldcd.co.uk>
2018-06-01 11:39:08 +0100

    s

--- playground/remove_interface.sh
+++ playground/remove_interface.sh
@@ -16,7 +16,7 @@ if [ ! -z "${iface_to_remove_bridge}" ] ; then
 echo "iface to remove bridge found - removing ${iface_to_remove_bridge}"
 sed -i "/${iface_to_remove_bridge}/,/^$/d" /etc/network/interfaces
 else
-
+echo "no bridge interfaces found for : ${iface_to_remove_bridge}"
 fi
 
 # Removes selected interface from /etc/network/interfaces
