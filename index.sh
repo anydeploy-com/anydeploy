bb227b17b5ade47437ff0b41fe032460453cd410
martin <martin@ldcd.co.uk>
2018-06-08 10:39:58 +0100

    update

--- index.sh
+++ index.sh
@@ -78,6 +78,29 @@ DEPLOY () {
 
 CAPTURE () {
   echo "TO BE DONE"
+
+  . /anydeploy/scripts/capture.sh
+
+  # Select Disk ( array + dialog)
+
+  # Mount + detect OS / Version / Codename / Revision / Architecture / Boot Type
+
+  # Generate name and prompt if okay
+
+  # Create folder with name
+
+  # Save textfile with name
+
+  # Save output of fdisk for partition information
+
+  # Save mbr
+
+  # Detect OS partition and prompt to resize
+
+  # Run Partclone for each partition and name them part1 part2 part3 etc.
+
+
+
 }
 
 TESTS () {
---
+++ scripts/capture.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "dupa"
+
+
+  # Select Disk ( array + dialog)
+
+  # Mount + detect OS / Version / Codename / Revision / Architecture / Boot Type
+
+  # Generate name and prompt if okay
+
+  # Create folder with name
+
+  # Save textfile with name
+
+  # Save output of fdisk for partition information
+
+  # Save mbr
+
+  # Detect OS partition and prompt to resize
+
+  # Run Partclone for each partition and name them part1 part2 part3 etc.
