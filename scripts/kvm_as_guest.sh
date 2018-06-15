b6d133a8e7c065d18622d5328b438547988683f7
martin <martin@ldcd.co.uk>
2018-06-15 17:23:40 +0100

    update

--- scripts/kvm_as_guest.sh
+++ scripts/kvm_as_guest.sh
@@ -8,20 +8,25 @@ apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils libguest
 
 mount -t tmpfs -o size=10m tmpfs /etc/network/interfaces.d
 
-# /etc/network/interfaces.d/anybr0
 
-auto lo
-iface lo inet loopback
 
-iface eno0 inet manual
 
-auto anybr0
-iface anybr0 inet static
-	address 192.168.1.128        ## set up/netmask/broadcast/gateway as per your setup
-	broadcast 192.168.1.255
-	netmask 255.255.255.0
-	gateway 192.168.1.254
-	bridge_ports eth0    # replace eth0 with your actual interface name
-	bridge_stp off       # disable Spanning Tree Protocol
-        bridge_waitport 0    # no delay before a port becomes available
-        bridge_fd 0          # no forwarding delay
+# /etc/network/interfaces.d/vmbr0
+
+# SETUP ROUTED BRIDGE
+
+auto vmbr0
+#private sub network
+iface vmbr0 inet static
+        address  10.10.10.1
+        netmask  255.255.255.0
+        bridge_ports none
+        bridge_stp off
+        bridge_fd 0
+
+        post-up echo 1 > /proc/sys/net/ipv4/ip_forward
+        post-up   iptables -t nat -A POSTROUTING -s '10.10.10.0/24' -o eno1 -j MASQUERADE
+        post-down iptables -t nat -D POSTROUTING -s '10.10.10.0/24' -o eno1 -j MASQUERADE
+
+
+# ENABLE POSTROUTING
